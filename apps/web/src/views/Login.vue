b6fcd153af401b93d4228fcf31dee14819b65d3c
LiBo <laird.lee@outlook.com>
2023-08-17 16:56:40 +0800

    feat：登录框架

---
+++ apps/web/src/views/Login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="login_bg"></div>
+</template>
+
+<style lang="less" scoped>
+.login_bg {
+  background: url('../assets/img/carpool_login.png') center center;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+}
+</style>
